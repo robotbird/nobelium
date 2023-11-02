e8af8a127e255ade06667d5242b6663b44a0f0bf
robotbird <robotbird@qq.com>
2023-11-02 11:53:57 +0800

    modifed lang

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'robotbird@qq.com',
   link: 'https://jqpress.com',
   description: '机器鸟的个人博客，希望在这个世界可以留下自己的作品',
-  lang: 'zh-HK', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
